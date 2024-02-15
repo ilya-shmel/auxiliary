a1664b87dab9db61fb8cbbf866fbcaff3ea9a695
Ilya Shmadchenko <prophet8912@gmail.com>
2024-02-15 15:01:09 +0300

    The 'copy_script': an array  was created.

--- copy_script.sh
+++ copy_script.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+SCRIPT_CONTENT=()
+SCRIPT_FILE="/opt/pangeoradar/support_tools/opensearch/migration.sh"
+
+## Convert the migration script into an array
+readarray -t SCRIPT_CONTENT < $SCRIPT_FILE    
+
+for (( KEY=0; KEY < ${#SCRIPT_CONTENT[@]}; ++KEY ))
+do
+    echo "$KEY" "${SCRIPT_CONTENT[KEY]}"
+done
