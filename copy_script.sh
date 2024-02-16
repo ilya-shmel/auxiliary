8c869898efebf95b0ecceed191508a87a21a3f72
Ilya Shmadchenko <prophet8912@gmail.com>
2024-02-16 14:37:18 +0300

    The 'copy_script.sh': the scp solution was added.

--- copy_script.sh
+++ copy_script.sh
@@ -7,24 +7,16 @@ ssh_function(){
 
     if [[ $IP == $(hostname -i) ]]
     then
-        "${COMMAND}" 2>/dev/null
+        eval "${COMMAND} 2>/dev/null"
     else
-        ssh -o 'StrictHostKeyChecking no' -i "$KEY_PATH" root@"$IP" "${COMMAND}" 2>/dev/null
+        eval "ssh -o 'StrictHostKeyChecking no' -i "$KEY_PATH" root@"$IP" "${COMMAND}" 2>/dev/null"
     fi
 }
 
 KEY_PATH="$HOME/.ssh/id_rsa"
-SCRIPT_CONTENT=()
 SCRIPT_FILE="/opt/pangeoradar/support_tools/opensearch/migration.sh"
+HOST_IP="172.30.254.84"
 CURRENT_NODE_IP="172.30.254.77"
 
-## Convert the migration script into an array
-readarray -t SCRIPT_CONTENT < $SCRIPT_FILE   
-
-## Create a new file for the script
-ssh_function $CURRENT_NODE_IP "touch /tmp/migration.sh"
-
-for (( KEY=0; KEY < ${#SCRIPT_CONTENT[@]}; ++KEY ))
-do
-    ssh_function $CURRENT_NODE_IP "${SCRIPT_CONTENT[KEY]} >> /tmp/migration.sh"
-done
+## Copy script to node
+ssh_function $CURRENT_NODE_IP "scp root@"$HOST_IP":$SCRIPT_FILE /tmp"
