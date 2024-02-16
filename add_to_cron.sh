e075ce7e7e52699634e4399e6d9bc54b352a3192
Ilya Shmadchenko <prophet8912@gmail.com>
2024-02-16 17:55:12 +0300

    The 'add_to_cron.sh' script: the script was approved.

--- add_to_cron.sh
+++ add_to_cron.sh
@@ -13,9 +13,21 @@ ssh_function(){
     fi
 }
 
+NUMBER_OF_SHARDS=3
 CURRENT_NODE_IP="172.30.254.77"
 INITIAL_HOST=$(hostname -i)
-SCRIPT_FILE="/root/migration.sh"
+SCRIPT_FILE="$HOME/migration.sh"
+CROND_FILE="$HOME/migration_job"
+CRON_LINE="*/1 * * * * root /root/migration.sh "$NUMBER_OF_SHARDS" >/dev/null 2>&1"
+KEY_PATH="$HOME/.ssh/id_rsa"
 
+## Prepare a cron file
+> "$CROND_FILE"
+echo "SHELL=/bin/sh" >> "$CROND_FILE"
+echo "PATH=/bin:/usr/bin:/sbin:/usr/sbin" >> "$CROND_FILE"
+echo "# Every minute start the migration script" >> "$CROND_FILE"
+echo "$CRON_LINE" >> "$CROND_FILE"
+
+## Copy the script and the cron file
 ssh_function $CURRENT_NODE_IP "scp root@"$INITIAL_HOST":"$SCRIPT_FILE" /root"
-ssh_function $CURRENT_NODE_IP "(crontab -u root -l; echo "*/5 * * * * root !/root/migration.sh "$NUMBER_OF_SHARDS" >/dev/null 2>&1") | crontab -u root -"
+ssh_function $CURRENT_NODE_IP "scp root@"$INITIAL_HOST":"$CROND_FILE" /etc/cron.d"
