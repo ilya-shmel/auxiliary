7051c3ff504784d44bdbf5fed986263b9f8c474c
Ilya Shmadchenko <prophet8912@gmail.com>
2024-03-12 10:49:02 +0300

    The 'agent_routine.sh' script: the sources.list staff was added.

--- agent_routine.sh
+++ agent_routine.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/bash
+SOURCE_LINES=(4 5 7 8)
+MASTER_NODE=$1
 
-apt install wget
+## Correct the sources.list - comment the third line and uncomment lines 4,5,7,8
+cd /etc/apt/
 
-## Install Log Navigator
-wget https://github.com/tstack/lnav/releases/download/v0.12.0/lnav-0.12.0-linux-musl-x86_64.zip
-unzip lnav-0.12.0-linux-musl-x86_64.zip
-cd lnav-0.12.0/ && cp lnav /usr/sbin
+sed '3 s/./#&/' sources.list >output.txt && mv output.txt sources.list
 
-## Set root aliases
-echo "alias lnav='/usr/sbin/lnav'" >> /root/.bashrc
-echo "alias ls='ls -lh'" >> /root/.bashrc
-. /root/.bashrc
+for LINE in ${SOURCE_LINES[@]}
+do
+    sed "$LINE s/^.//" sources.list >output.txt && mv output.txt sources.list
+done
 
-## Delete temporal directories
-rm lnav-0.12.0-linux-musl-x86_64.zip
-rm lnav-0.12.0/
+#apt install wget
+#
+### Install Log Navigator
+#wget https://github.com/tstack/lnav/releases/download/v0.12.0/lnav-0.12.0-linux-musl-x86_64.zip
+#unzip lnav-0.12.0-linux-musl-x86_64.zip
+#cd lnav-0.12.0/ && cp lnav /usr/sbin
+#
+### Set root aliases
+#echo "alias lnav='/usr/sbin/lnav'" >> /root/.bashrc
+#echo "alias ls='ls -lh'" >> /root/.bashrc
+#. /root/.bashrc
+#
+### Delete temporal directories
+#rm lnav-0.12.0-linux-musl-x86_64.zip
+#rm lnav-0.12.0/
